576cda97884881629ee3bb9acdbbf86bda7f82e0
kanziw <kanziwoong@gmail.com>
2017-03-11 00:55:37 +0900

    mealKey 가 정상적으로 파싱되지 않던 버그 수정

--- src/const.js
+++ src/const.js
@@ -1,10 +1,15 @@
 export const Timezone = 'Asia/Seoul'
 
-export const LunchHHmm = '11:00'    // Lunch's starting time
-export const DinnerHHmm = '17:00'   // Dinner's starting time
+export const HHmm = {
+  Morning: '06:00',       // Morning's starting time
+  Lunch: '11:00',         // Lunch's starting time
+  Dinner: '17:00',        // Dinner's starting time
+  LateNightMeal: '21:00'  // LateNightMeal's starting time
+}
 
 export const MealKey = {
   Morning: 'Morning',
   Lunch: 'Lunch',
   Dinner: 'Dinner',
+  LateNightMeal: 'LateNightMeal',
 }
