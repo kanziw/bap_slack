f0f44f2f91b695a664899c4f536f9411bdb8bb5d
red <seok20@gmail.com>
2017-03-20 23:11:53 +0900

    동작확인 테스트 완료.

--- src/const.js
+++ src/const.js
@@ -1,11 +1,13 @@
 export const Timezone = 'Asia/Seoul'
 
+export const LunchHHmmss = '23:10:20'    // Lunch's starting time
+export const DinnerHHmmss = '18:20:00'   // Dinner's starting time
 export const ChannelNameForNoti = 'bap'
 
 export const HHmm = {
-  Morning: '06:00:00',       // Morning's starting time
-  Lunch: '12:20:00',         // Lunch's starting time
-  Dinner: '18:20:00',        // Dinner's starting time
+  Morning: '06:00',       // Morning's starting time
+  Lunch: '12:20',         // Lunch's starting time
+  Dinner: '18:20',        // Dinner's starting time
   LateNightMeal: '21:00'  // LateNightMeal's starting time
 }
 
